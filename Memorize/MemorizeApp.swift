a97b7df471fc8b5e356a749c9feb9e35abd84171
曹光耀 <caoguangyao@chinasofti.com>
2022-03-05 19:25:44 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 曹光耀 on 2022/3/5.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
