90f69bb547ca2bf3e7fa5697b34717bce8de583b
曹光耀 <caoguangyao@chinasofti.com>
2022-03-07 14:39:13 +0800

    优化大量的循环代码 为 函数式编码调用，以及 各种简略写法

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -10,7 +10,41 @@ import Foundation
 struct MemoryGame<CardContent> where CardContent: Equatable {
 //  私有但外部可以看到，外部不可以修改
   private(set) var cards: Array<Card>
-  private var indexOfTheOneAndOnlyFaceUpCard: Int?
+  private var indexOfTheOneAndOnlyFaceUpCard: Int? {
+    // 利用计算属性 完成 计算
+    get { // 访问变量时 触发
+//      var faceUpCardIndices = [Int]()
+//      for index in cards.indices {
+//        if cards[index].isFaceUp {
+//          faceUpCardIndices.append(index)
+//        }
+//      }
+      // 进一步优化
+      let faceUpCardIndices = cards.indices.filter({ cards[$0].isFaceUp }) // index in cards[index].isFaceUp
+      
+//      if faceUpCardIndices.count == 1 {
+//        return faceUpCardIndices.first
+//      } else {
+//        return nil
+//      }
+      
+      // 拓展一个Array 属性-计算属性：判断数组长度 返回值
+      return faceUpCardIndices.oneAndOnly
+      // 还可以简化为链式调用 且单行时还可以省略 return
+      // return faceUpCardIndices = cards.indices.filter({ cards[$0].isFaceUp }).oneAndOnly
+    }
+    set { // 外部进行变量赋值 时触发
+//      for index in cards.indices {
+//        if index != newValue {
+//          cards[index].isFaceUp = false
+//        } else {
+//          cards[index].isFaceUp = true
+//        }
+//      }
+      // 简化为
+      cards.indices.forEach({ cards[$0].isFaceUp = ($0 == newValue) })
+    }
+  }
   // mutating 修饰 方法可以修改 struct 内变量
   mutating func choose(_ card: Card) {
     // struct 是值类型  swift 赋值 函数入参都是 拷贝 不存在引用
@@ -25,14 +59,16 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
           cards[chooseIndex].isMatched = true
           cards[potentialMacthIndex].isMatched = true
         }
-        indexOfTheOneAndOnlyFaceUpCard = nil
+//        indexOfTheOneAndOnlyFaceUpCard = nil
+        cards[chooseIndex].isFaceUp = true
+        
       } else {
-        for index in 0..<cards.count {
-          cards[index].isFaceUp = false
-        }
+//        for index in 0..<cards.count { 提炼到计算属性 里了
+//          cards[index].isFaceUp = false
+//        }
         indexOfTheOneAndOnlyFaceUpCard = chooseIndex
       }
-      cards[chooseIndex].isFaceUp.toggle()
+//      cards[chooseIndex].isFaceUp.toggle()
     }
     
   }
@@ -65,5 +101,16 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     var isMatched = false
     let content: CardContent // 来自 MemoryGame 的 CardContent 同样由外部初始化时传入决定
     let id: Int
+  }
+}
+
+// 给Array 拓展一个属性
+extension Array {
+  var oneAndOnly: Element? {
+    if count == 1 {
+      return first
+    } else {
+      return nil
+    }
   }
 }
