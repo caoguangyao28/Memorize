ac3997276f873d33c6a3c36643d316a4b83308cf
曹光耀 <caoguangyao@chinasofti.com>
2022-03-09 15:45:44 +0800

    自定义 Pie 图形，使用shape 协议

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -46,6 +46,9 @@ struct CardView: View {
         if card.isFaceUp {
           shape.fill().foregroundColor(.white)
           shape.strokeBorder(lineWidth: DrawingConstans.lineWidth)
+          // shape circle
+//          Circle().padding(5).opacity(0.5)
+          Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110 - 90)).padding(5).opacity(0.5)
           Text(card.content).font(font(in: geometry.size))
         } else if card.isMatched {
           shape.opacity(0)
@@ -64,7 +67,7 @@ struct CardView: View {
   private struct DrawingConstans {
     static let cornerRadius:CGFloat = 10
     static let lineWidth:CGFloat = 3
-    static let fontScale:CGFloat = 0.7
+    static let fontScale:CGFloat = 0.65
   }
 }
 
@@ -104,7 +107,8 @@ struct CardView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
       let game = EmojiMemoryGame()
-      EmojiMemoryGameView(game: game)//.preferredColorScheme(.dark)
+      game.choose(game.cards.first!)
+      return EmojiMemoryGameView(game: game)//.preferredColorScheme(.dark)
 //      EmojiMemoryGameView(viewModle: game).preferredColorScheme(.light)
     }
 }
---
+++ Memorize/Pie.swift
@@ -0,0 +1,40 @@
+//
+//  Pie.swift
+//  Memorize
+//  自定义图形 拓展 协议 shape
+//  Created by 曹光耀 on 2022/3/9.
+//
+
+import SwiftUI
+
+struct Pie: Shape {
+  var startAngle: Angle
+  var endAngle: Angle
+  var clockwise: Bool = false
+  
+  func path(in rect: CGRect) -> Path {
+    let center = CGPoint(x: rect.midX, y: rect.midY)
+    let radius = min(rect.width, rect.height) / 2
+    let start = CGPoint(
+      x: center.x + radius * cos(startAngle.radians),
+      y: center.y + radius * sin(startAngle.radians)
+    )
+    
+    var p = Path()
+    p.move(to: center)
+    p.addLine(to: start)
+    // clockwise 需要取反 ，程序中的坐标系 与 真实的物理坐标系 是不一致的
+    p.addArc(
+      center: center,
+      radius: radius,
+      startAngle: startAngle,
+      endAngle: endAngle,
+      clockwise: !clockwise
+    )
+    p.addLine(to: center)
+    
+    return p
+  }
+  
+  
+}
